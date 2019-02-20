bba966bb528eb7fa21d445989f8a4a737453e283
nachman-shelef <nachman.shelef@gmail.com>
2019-02-20 14:17:39 +0200

    muni names from hebrew to english

--- python/muni_scores.py
+++ python/muni_scores.py
@@ -7,7 +7,7 @@
 import transitanalystisrael_config as cfg
 import muni_tpd_at_stops_v3_w_10x_for_rail
 import muni_opd_from_stops_tpd
-import muni_transitscore_from_opd_v1
+import muni_transitscore_from_opd_v2
 import time
 #
 print "Local current time :", time.asctime( time.localtime(time.time()) )
@@ -16,6 +16,6 @@ print "Local current time :", time.asctime( time.localtime(time.time()) )
 
 muni_tpd_at_stops_v3_w_10x_for_rail.main(cfg.gtfsdate, cfg.gtfspath, cfg.gtfsdirbase, cfg.processedpath, cfg.serviceweekstartdate)
 muni_opd_from_stops_tpd.main(cfg.gtfsdate, cfg.processedpath, cfg.serviceweekstartdate)
-muni_transitscore_from_opd_v1.main(cfg.gtfsdate, cfg.processedpath, cfg.serviceweekstartdate)
+muni_transitscore_from_opd_v2.main(cfg.gtfsdate, cfg.processedpath, cfg.serviceweekstartdate, cfg.language)
 
 print "Local current time :", time.asctime( time.localtime(time.time()) )
