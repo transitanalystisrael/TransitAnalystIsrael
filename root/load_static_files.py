98f7d00058fb31aeafec3c87ba0edf9c117f6b61
nachman-shelef <nachman.shelef@gmail.com>
2019-02-20 16:52:16 +0200

    added static files dir and script to copy files to processed

---
+++ root/load_static_files.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# copy static data files from static_data dir to processed dir 
+#
+import transitanalystisrael_config as cfg
+import shutil
+import os
+
+srcdir = cfg.staticpath
+dstdir = cfg.processedpath
+
+os.chdir(srcdir)
+print srcdir
+print dstdir
+print os.listdir(srcdir)
+srcdirfilelist = os.listdir(srcdir)
+for filename in srcdirfilelist :
+	print filename
+	filein = srcdir+filename
+	fileout = dstdir+filename
+	shutil.copyfile(filein,fileout)
+print os.listdir(dstdir)
+
+
+
+
