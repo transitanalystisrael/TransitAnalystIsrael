f04f55a1bccc747e6fcfbb0a3063ebf02cd863b8
nachman-shelef <nachman.shelef@gmail.com>
2019-02-23 17:24:11 +0200

    give 10x5x3x weight to trains, lrt, brt by using stop_types.txt

--- root/muni_scores.py
+++ root/muni_scores.py
@@ -5,7 +5,7 @@
 #
 
 import transitanalystisrael_config as cfg
-import muni_tpd_at_stops_v3_w_10x_for_rail
+import muni_tpd_at_stops_w_10x5x3x_for_rail_lrt_brt
 import muni_opd_from_stops_tpd
 import muni_transitscore_from_opd_v2
 import time
@@ -14,7 +14,7 @@ print "Local current time :", time.asctime( time.localtime(time.time()) )
 #
 #
 
-muni_tpd_at_stops_v3_w_10x_for_rail.main(cfg.gtfsdate, cfg.gtfspath, cfg.gtfsdirbase, cfg.processedpath, cfg.serviceweekstartdate)
+muni_tpd_at_stops_w_10x5x3x_for_rail_lrt_brt.main(cfg.gtfsdate, cfg.gtfspath, cfg.gtfsdirbase, cfg.processedpath, cfg.serviceweekstartdate)
 muni_opd_from_stops_tpd.main(cfg.gtfsdate, cfg.processedpath, cfg.serviceweekstartdate)
 muni_transitscore_from_opd_v2.main(cfg.gtfsdate, cfg.processedpath, cfg.serviceweekstartdate, cfg.language)
 
