4635c3e8d80a3113bb539a52c23e309761fd2b15
nachman-shelef <nachman.shelef@gmail.com>
2019-02-27 14:07:29 +0200

    added use of config parameter get_service_date = 'auto' or 'on_demand' to upload files into the appropriate destination bucket in aws s3

--- root/upload2aws_s3.py
+++ root/upload2aws_s3.py
@@ -8,14 +8,6 @@ import shutil
 import os
 import boto3
 
-print('********** upload to AWS S3 cloud *************')
-current_localdir = cfg.websitelocalcurrentpath[:-1]
-temp_localdir = cfg.temppath
-print('current_localdir : ',current_localdir)
-
-os.chdir(current_localdir)
-print(os.listdir(current_localdir))
-
 def delete_all_objects(bucket_name):
 	print('--------delete_all_objects------------')
 	print(bucket_name)
@@ -112,106 +104,153 @@ def upload_localdir_w_gzip_to_bucket(localdir_from, bucket_to_name):
 
 #-------------------------------------------------------------------------
 
-# Create an S3 resource
-s3 = boto3.resource('s3')
+if cfg.get_service_date == 'auto' : 
+	print('********** upload to AWS S3 cloud *************')
+	current_localdir = cfg.websitelocalcurrentpath[:-1]
+	temp_localdir = cfg.temppath
+	print('current_localdir : ',current_localdir)
 
-# Call S3 to list current buckets
-for bucket in s3.buckets.all():
-	print((bucket.name))
-#	for key in bucket.objects.all():
-#		print(key.key)
+	os.chdir(current_localdir)
+	print(os.listdir(current_localdir))
 
-#
-# Create an Amazon S3 Bucket - not needed - buckets are pre-created
-#
-#s3.create_bucket(Bucket='transitanalystisrael-current', CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
+	# Create an S3 resource
+	s3 = boto3.resource('s3')
 
-#
-# erase content of backup 
-#
-print_objects('transitanalystisrael-backup')
-delete_all_objects('transitanalystisrael-backup')
+	# Call S3 to list current buckets
+	for bucket in s3.buckets.all():
+		print((bucket.name))
+	#	for key in bucket.objects.all():
+	#		print(key.key)
 
-#
-# copy content of past to backup
-#
-print_objects('transitanalystisrael-past')
-copy_bucket('transitanalystisrael-past', 'transitanalystisrael-backup')
-print_objects('transitanalystisrael-backup')
+	#
+	# Create an Amazon S3 Bucket - not needed - buckets are pre-created
+	#
+	#s3.create_bucket(Bucket='transitanalystisrael-current', CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
 
-#
-# erase content of past
-#
-print_objects('transitanalystisrael-past')
-delete_all_objects('transitanalystisrael-past')
+	#
+	# erase content of backup 
+	#
+	print_objects('transitanalystisrael-backup')
+	delete_all_objects('transitanalystisrael-backup')
 
-#
-# copy content of current to past
-#
-print_objects('transitanalystisrael-current')
-copy_bucket('transitanalystisrael-current', 'transitanalystisrael-past')
-print_objects('transitanalystisrael-past')
+	#
+	# copy content of past to backup
+	#
+	print_objects('transitanalystisrael-past')
+	copy_bucket('transitanalystisrael-past', 'transitanalystisrael-backup')
+	print_objects('transitanalystisrael-backup')
 
-#
-# erase content of current
-#
-print_objects('transitanalystisrael-current')
-delete_all_objects('transitanalystisrael-current')
+	#
+	# erase content of past
+	#
+	print_objects('transitanalystisrael-past')
+	delete_all_objects('transitanalystisrael-past')
 
-#
-# copy content of local to current
-#
-#upload_localdir_w_gzip_to_bucket(for_testing_upload_localdir, 'transitanalystisrael-current') # for testing
-upload_localdir_w_gzip_to_bucket(current_localdir, 'transitanalystisrael-current')
-print_objects('transitanalystisrael-current')
+	#
+	# copy content of current to past
+	#
+	print_objects('transitanalystisrael-current')
+	copy_bucket('transitanalystisrael-current', 'transitanalystisrael-past')
+	print_objects('transitanalystisrael-past')
 
-print('------------------------------------------------------------')
-# Call S3 to list current buckets
-for bucket in s3.buckets.all():
-	print((bucket.name))
-#	for key in bucket.objects.all():
-#		print(key.key)
+	#
+	# erase content of current
+	#
+	print_objects('transitanalystisrael-current')
+	delete_all_objects('transitanalystisrael-current')
 
-#
-# change current_or_past in js config file to past in transitanalystisrael-past bucket
-# this enables the TTM client to point to the correct server
-#
-# download js config file to temp dir, edit file, then upload
-#download
-s3.Bucket('transitanalystisrael-past').download_file('docs/transitanalystisrael_config.js', temp_localdir+'transitanalystisrael_config.js')
-#edit
-jsfile = 'transitanalystisrael_config.js'
-tempjsfile = 'temp_config.js'
-in_dir = temp_localdir
-out_dir = temp_localdir
-maxfilelinecount = 2000
-print('input from ', in_dir+jsfile)
-print('output to ', out_dir+tempjsfile)
-filein = open(in_dir+jsfile, 'r', encoding="utf8")
-fileout = open(out_dir+tempjsfile, 'w', encoding="utf8")
-count = 0
-sline = filein.readline()
-while ((count < maxfilelinecount) and (sline != '')):
-	if sline.find('var cfg_current_or_past') == 0 : 
-		postsline = sline.replace("'current'","'past'")
-		fileout.write(postsline)
-	else :
-		postsline = sline
-		fileout.write(postsline)
-	#print len(sline), sline
-	count +=1
+	#
+	# copy content of local to current
+	#
+	#upload_localdir_w_gzip_to_bucket(for_testing_upload_localdir, 'transitanalystisrael-current') # for testing
+	upload_localdir_w_gzip_to_bucket(current_localdir, 'transitanalystisrael-current')
+	print_objects('transitanalystisrael-current')
+
+	print('------------------------------------------------------------')
+	# Call S3 to list current buckets
+	for bucket in s3.buckets.all():
+		print((bucket.name))
+	#	for key in bucket.objects.all():
+	#		print(key.key)
+
+	#
+	# change current_or_past in js config file to past in transitanalystisrael-past bucket
+	# this enables the TTM client to point to the correct server
+	#
+	# download js config file to temp dir, edit file, then upload
+	#download
+	s3.Bucket('transitanalystisrael-past').download_file('docs/transitanalystisrael_config.js', temp_localdir+'transitanalystisrael_config.js')
+	#edit
+	jsfile = 'transitanalystisrael_config.js'
+	tempjsfile = 'temp_config.js'
+	in_dir = temp_localdir
+	out_dir = temp_localdir
+	maxfilelinecount = 2000
+	print('input from ', in_dir+jsfile)
+	print('output to ', out_dir+tempjsfile)
+	filein = open(in_dir+jsfile, 'r', encoding="utf8")
+	fileout = open(out_dir+tempjsfile, 'w', encoding="utf8")
+	count = 0
 	sline = filein.readline()
-print('------------------')
-print(' infile line count ',count)
-filein.close()
-fileout.close()
-print('closed ', in_dir+jsfile)
-print('closed ', out_dir+tempjsfile)
-shutil.copyfile(out_dir+tempjsfile,out_dir+jsfile)
-os.remove(out_dir+tempjsfile)
-#upload
-with open(out_dir+jsfile, 'rb') as data:
-	s3.Bucket('transitanalystisrael-past').put_object(Key='docs/transitanalystisrael_config.js', Body=data, ContentType='application/javascript')
-	print('uploaded : ', out_dir+jsfile)
+	while ((count < maxfilelinecount) and (sline != '')):
+		if sline.find('var cfg_current_or_past') == 0 : 
+			postsline = sline.replace("'current'","'past'")
+			fileout.write(postsline)
+		else :
+			postsline = sline
+			fileout.write(postsline)
+		#print len(sline), sline
+		count +=1
+		sline = filein.readline()
+	print('------------------')
+	print(' infile line count ',count)
+	filein.close()
+	fileout.close()
+	print('closed ', in_dir+jsfile)
+	print('closed ', out_dir+tempjsfile)
+	shutil.copyfile(out_dir+tempjsfile,out_dir+jsfile)
+	os.remove(out_dir+tempjsfile)
+	#upload
+	with open(out_dir+jsfile, 'rb') as data:
+		s3.Bucket('transitanalystisrael-past').put_object(Key='docs/transitanalystisrael_config.js', Body=data, ContentType='application/javascript')
+		print('uploaded : ', out_dir+jsfile)
+#---------------------------------------------------------------------------------------
+else : # cfg.get_service_date == 'on_demand'
+	on_demand_dir = cfg.websitelocalondemandpath.replace('yyyymmdd', cfg.gtfsdate)
+	print('********** upload to AWS S3 cloud *************')
+	on_demand_localdir = on_demand_dir
+	temp_localdir = cfg.temppath
+	print('on_demand_localdir : ',on_demand_localdir)
 
+	os.chdir(on_demand_localdir)
+	print(os.listdir(on_demand_localdir))
+
+	# Create an S3 resource
+	s3 = boto3.resource('s3')
+
+	# Call S3 to list current buckets
+	for bucket in s3.buckets.all():
+		print((bucket.name))
+	#	for key in bucket.objects.all():
+	#		print(key.key)
+
+	#
+	# Create an Amazon S3 Bucket - needed for on demand. for auto - buckets are pre-created
+	#
+	on_demand_bucket = 'transitanalystisrael-'+cfg.gtfsdate
+	s3.create_bucket(Bucket=on_demand_bucket, CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
+
+	#
+	# copy content of local to on_demand bucket
+	#
+	#upload_localdir_w_gzip_to_bucket(for_testing_upload_localdir, 'transitanalystisrael-current') # for testing
+	upload_localdir_w_gzip_to_bucket(on_demand_localdir, on_demand_bucket)
+	print_objects(on_demand_bucket)
+
+	print('------------------------------------------------------------')
+	# Call S3 to list current buckets
+	for bucket in s3.buckets.all():
+		print((bucket.name))
+	#	for key in bucket.objects.all():
+	#		print(key.key)
 
