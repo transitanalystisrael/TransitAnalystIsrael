500f8b322ef7a01227f7c8e6fb091bb5cdc8a53b
nachman-shelef <nachman.shelef@gmail.com>
2019-03-13 20:35:39 +0200

    no message

--- root/transitanalystisrael_config.py
+++ root/transitanalystisrael_config.py
@@ -36,18 +36,17 @@
 #ttm_server_on = 'none'
 
 #On demand date on local pc
-#get_service_date = 'on_demand'
-#python_processing = 'local_pc'
-#ttm_graph_processing = 'local_pc'
-#web_client_hosted_on = 'local_pc'
-#ttm_server_on = 'local_pc'
-
-#On demand date on local pc no TTM
 get_service_date = 'on_demand'
 python_processing = 'local_pc'
-ttm_graph_processing = 'none'
+ttm_graph_processing = 'local_pc'
 web_client_hosted_on = 'local_pc'
-ttm_server_on = 'none'
+ttm_server_on = 'local_pc'
+
+#On demand date on local pc no TTM
+#python_processing = 'local_pc'
+#ttm_graph_processing = 'none'
+#web_client_hosted_on = 'local_pc'
+#ttm_server_on = 'none'
 
 
 # common config
