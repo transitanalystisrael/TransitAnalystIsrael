d7749b97e33f882d28350dac03d70df6f8ee4779
Shaked Kaufmann <shakedk@gmail.com>
2019-04-04 14:16:32 +0300

    Fixing UTC time issues on AWS

--- root/display_update_notification.py
+++ root/display_update_notification.py
@@ -4,7 +4,7 @@ toaster = ToastNotifier()
 path = Path.cwd()
 toaster.show_toast("Transit Analyst Israel Update", "Transit Analyst Israel will be updated at 22:30."
                                                     " Please leave your machine on and plugged-in."
-                                                    " Navitia Server on Docker should be running before the update.",
+                                                    " Docker Desktop should be restarted and Navitia server running.",
                    icon_path= path.parent / "website_no_data" / "docs" / "images" / "favicon.ico",
                    duration="300")
 
