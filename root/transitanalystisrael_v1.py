645e9fe0683edfdeb65bc3d8193c702beee1c330
nachman-shelef <nachman.shelef@gmail.com>
2019-07-05 11:36:01 +0300

    moved muni_scores.py to be after stops_in_muni_pre2post_edit.py - so it can use the list of stops in muni created (instead of filtering stops by muni poly again)

--- root/transitanalystisrael_v1.py
+++ root/transitanalystisrael_v1.py
@@ -13,6 +13,7 @@ import process_date
 update_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
 
 try:
+    
     if cfg.get_service_date == 'auto':
         next_month_operation_date = process_date.get_auto_date_nextmonth()  # The date that the product should already be working
         next_month_operation_date = datetime.datetime.strptime(next_month_operation_date, '%Y%m%d')
@@ -43,8 +44,6 @@ try:
     import gtfs_preprocessing
     _log.info("Calculating TransitScore Israel")
     import transitscore_israel # check that long processing steps (2 hours)are not commented out in imported file
-    _log.info("Calculating Municipal Scores")
-    import muni_scores
     _log.info("Calculating High Freq Lines")
     import high_freq_lines_israel
     _log.info("Calculating Lines on Street")
@@ -55,6 +54,8 @@ try:
     import stops_in_muni_pre_edit
     _log.info("Stops in Muni - Post Edit")
     import stops_in_muni_pre2post_edit # you can also manually edit the pre file to create the post file and rerun the script with this commented out
+    _log.info("Calculating Municipal Scores")
+    import muni_scores
     _log.info("Calculating TPD in Municipal")
     import tpd_in_muni_per_line
     _log.info("Stops near train stops - Pre edit")
@@ -80,7 +81,7 @@ try:
         #upload files to cloud website dir from local website dir
         _log.info("Upload website to AWS S3")
         import upload2aws_s3
-
+    
     if cfg.ttm_graph_processing != 'none':
         # process TTM files
         _log.info("Update Navitia Time Map server")
